ceb73c7a02be745051658d2dd6bc23168cc85f8e
Daniel Tomás Ozalla <ticidici@yahoo.es>
2023-12-29 13:07:50 +0100

    capsule climb trace

--- Source/ClimbingSystem/Public/Components/CustomMovementComponent.h
+++ Source/ClimbingSystem/Public/Components/CustomMovementComponent.h
@@ -13,5 +13,38 @@ UCLASS()
 class CLIMBINGSYSTEM_API UCustomMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
+
+public:
 	
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	
+private:
+	
+#pragma region ClimbTraces
+
+	TArray<FHitResult> DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End, bool bShowDebugShape = false);
+	
+#pragma endregion
+
+	
+#pragma region ClimbCore
+
+	void TraceClimbableSurfaces();
+	
+#pragma endregion
+
+	
+#pragma region ClimbVariables
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement: Climbing", meta = (AllowPrivateAccess = "true"))
+	TArray<TEnumAsByte<EObjectTypeQuery>> ClimbableSurfaceTraceType;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement: Climbing", meta = (AllowPrivateAccess = "true"))
+	float ClimbCapsuleTraceRadius = 50.f;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement: Climbing", meta = (AllowPrivateAccess = "true"))
+	float ClimbCapsuleTraceHalfHeight = 72.f;
+	
+#pragma endregion
+
 };
